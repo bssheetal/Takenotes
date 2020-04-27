784e42df936133236eeb2f2b073f21426306b0c8
Sheetal <sheetal.srikumar@gmail.com>
2020-04-27 16:46:55 -0700

    delete works and get issue fixed

---
+++ routes/apiroutes.js
@@ -0,0 +1,25 @@
+var router = require("express").Router();
+const store = require("../db/store");
+// GET "/api/notes" responds with all notes from the database
+router.get("/notes", function(req, res) {
+    store
+      .getNotes()
+      .then(notes => res.json(notes))
+      .catch(err => res.status(500).json(err));
+  });
+  
+  router.post("/notes", (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch(err => res.status(500).json(err));
+  });
+
+  router.delete("/notes/:id",(req,res)=>{
+    store.deleteNote(req.params.id)
+    .then(res.send(200))//send sucess status to browser
+      .catch(err => res.status(500).json(err));
+  });
+
+  
+  module.exports = router;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,14 @@
 var path = require("path");
 var router = require("express").Router();
 
-router.get("*",function(req,res)
-{
-    res.sendFile(path.join(__dirname,"../public/index.html"));
+// "/notes" responds with the notes.html file
+router.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-router.get("/notes",function(req,res)
-{
-   res.sendFile(path.join(__dirname,"../public/notes.html"));
+// All other routes respond with the index.html file
+router.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+module.exports = router;
